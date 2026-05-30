7102339269d2addf7b8d62f4bf02b11d0a01bb3e
code-practice-trail <tenali.ravikumar4@gmail.com>
2026-05-30 17:18:45 +0530

    Update sp_create_customer.sql

--- Procedures/sp_create_customer.sql
+++ Procedures/sp_create_customer.sql
@@ -1,1 +1,35 @@
 
+USE [UPI_project]
+GO
+
+/****** Object:  StoredProcedure [trans_schema].[sp_create_customer]    Script Date: 30-05-2026 17:17:36 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE OR ALTER  procedure [trans_schema].[sp_create_customer] 
+(
+		   @p_PERSON_ID bigint,
+           @p_BANK_ID int
+)
+as
+begin
+INSERT INTO [trans_schema].[CUSTOMER]
+           ([PERSON_ID]
+           ,[BANK_ID]
+           ,[CUSTOMER_CODE]
+           ,[KYC_STATUS]
+           ,[CREATED_AT])
+     VALUES
+           (@p_PERSON_ID,
+           @p_BANK_ID ,
+           CONCAT('CUST',NEXT VALUE FOR
+        trans_schema.SEQ_CUSTOMER_CODE),
+           'APPROVED',
+			current_timestamp);
+end;
+GO
+
